28a78f4c21520e59a33209fca593579fbcfb4377
martypdx <marty@martypdx.com>
2018-04-02 15:15:28 -0700

    working some method

--- week01_javascript-node-fundementals/class01_node-tdd/array-methods/lib/some.js
+++ week01_javascript-node-fundementals/class01_node-tdd/array-methods/lib/some.js
@@ -2,7 +2,7 @@
 module.exports = function some(arr, callback) {
     for(let i = 0; i < arr.length; i++) {
         const result = callback(arr[i]);
-        if(callback(arr[i])) return true;
+        if(result) return true;
     }
     return false;
 }
--- week01_javascript-node-fundementals/class01_node-tdd/array-methods/test/some.test.js
+++ week01_javascript-node-fundementals/class01_node-tdd/array-methods/test/some.test.js
@@ -2,15 +2,28 @@ const assert = require('assert');
 const some = require('../lib/some');
 
 describe('some', () => {
+
+    const isEven = x => x % 2 === 0;
     it('returns false if no elements match', () => {
         const arr = [1, 3, 5, 7];
-        const hasEven = some(arr, x => x % 2 === 0);
+        const hasEven = some(arr, isEven);
         assert.equal(hasEven, false);
     });
 
     it('returns true if at least one element matches', () => {
         const arr = [1, 3, 4, 7];
-        const hasEven = some(arr, x => x % 2 === 0);
+        const hasEven = some(arr, isEven);
         assert.equal(hasEven, true);
-    })
+    });
+
+    it('some short-circuits when one element is true', () => {
+        const arr = [3, 2, 5, 6];
+        const called = [];
+        some(arr, x => {
+            called.push(x);
+            return isEven(x);
+        });
+        assert.deepEqual(called, [2, 3]);
+
+    });
 });
