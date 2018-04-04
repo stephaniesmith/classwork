10bdaa78c44e3a6da55733d28545f3a831330c03
martypdx <marty@martypdx.com>
2018-04-04 14:32:53 -0700

    remove unneeded test suite

--- week01_javascript-node-fundementals/class03_buffer-binary/test/cool-stringifier.test.js
+++ week01_javascript-node-fundementals/class03_buffer-binary/test/cool-stringifier.test.js
@@ -3,32 +3,29 @@ const CoolStringifier = require('../lib/cool-stringifier');
 
 describe('cool stringifier', () => {
 
-    describe('basic operations', () => {
+    const sentence = 'This is so cool';
+    let stringifier = null;
+    beforeEach(() => {
+        stringifier = new CoolStringifier(sentence);
+    });
 
-        const sentence = 'This is so cool';
-        let stringifier = null;
-        beforeEach(() => {
-            stringifier = new CoolStringifier(sentence);
-        });
+    it('reverse each word', () => {
+        stringifier.reverseWords();
+        assert.equal(stringifier.sentence, 'sihT si os looc');
+    });
 
-        it('reverse each word', () => {
-            stringifier.reverseWords();
-            assert.equal(stringifier.sentence, 'sihT si os looc');
-        });
+    it('reverses word order', () => {
+        stringifier.reverseWordOrder();
+        assert.equal(stringifier.sentence, 'cool so is This');
+    });
 
-        it('reverses word order', () => {
-            stringifier.reverseWordOrder();
-            assert.equal(stringifier.sentence, 'cool so is This');
-        });
+    it('uppercase all things', () => {
+        stringifier.shout();
+        assert.equal(stringifier.sentence, 'THIS IS SO COOL');
+    });
 
-        it('uppercase all things', () => {
-            stringifier.shout();
-            assert.equal(stringifier.sentence, 'THIS IS SO COOL');
-        });
-
-        it('chains methods', () => {
-            stringifier.reverseWords().shout();
-            assert.equal(stringifier.sentence, 'SIHT SI OS LOOC');        
-        });
+    it('chains methods', () => {
+        stringifier.reverseWords().shout();
+        assert.equal(stringifier.sentence, 'SIHT SI OS LOOC');
     });
 });
