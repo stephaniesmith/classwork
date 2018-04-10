de550a4fcb2d938773519321531a79d3f04011c3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-04-10 14:24:26 -0700

    created sever

--- week02_Node-server-fundementals/class07_HTTP-REST/server.js
+++ week02_Node-server-fundementals/class07_HTTP-REST/server.js
@@ -1,1 +1,11 @@
+const app = require('./lib/app');
+const http = require('http');
 
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log('server started', server.address().port);
+});
